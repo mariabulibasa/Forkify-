1b33083284948f21921187243978ebe767412c77
mariabulibasa <maria.bulibasa14@gmail.com>
2026-04-19 14:02:27 +0300

    fixed bug with id

--- src/js/views/previewView.js
+++ src/js/views/previewView.js
@@ -9,8 +9,7 @@ class PreviewView extends View {
 
     return `
         <li class="preview">
-            <a class="preview__link" ${this._data.id === id ? 'preview__link--active' : ''} href="#${this._data.id}">
-              <figure class="preview__fig">
+          <a class="preview__link ${this._data.id === id ? 'preview__link--active' : ''}" href="#${this._data.id}">              <figure class="preview__fig">
                 <img src="${this._data.image}" alt="${this._data.title}" />
               </figure>
               <div class="preview__data">
